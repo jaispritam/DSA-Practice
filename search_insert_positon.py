c527ef2a09e4e35905bbbba9d5504d8cb56f3c73
Pritam Jaiswal <pritamjais006@gmail.com>
2025-12-06 23:50:26 +0530

    Create search_insert_positon.py

---
+++ search_insert_positon.py
@@ -0,0 +1,19 @@
+#leet code
+# 35. Search insert positon
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
