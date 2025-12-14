8560f41d0e6708aa6cc376b4055f55d35d419f2a
Pritam Jaiswal <pritamjais006@gmail.com>
2025-12-14 21:38:21 +0530

    Create Pow(x,n).py

---
+++ Pow(x,n).py
@@ -0,0 +1,17 @@
+# leetcode
+# 50. Pow(x, n)
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+
+        def function(base=x, exponent=abs(n)):
+            if exponent == 0:
+                return 1
+            elif exponent % 2 == 0:
+                return function(base * base, exponent // 2)
+            else:
+                return base * function(base * base, (exponent - 1) // 2)
+
+        f = function()
+        
+        return float(f) if n >= 0 else 1/f
